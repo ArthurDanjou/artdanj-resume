0c453dd63ae9c903ccfdbc5df79aa7165c492dfa
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-07-12 18:48:32 +0200

    Add new tech in stack section

--- src/components/StackSection.vue
+++ src/components/StackSection.vue
@@ -4,6 +4,6 @@
     <Stack title="stacks.languages" content="TypeScript, JavaScript, Python, Java, Rust" />
     <Stack title="stacks.frontend" content="VueJs, NuxtJs, WindiCss, Sass, TauriApp, ViteJs" />
     <Stack title="stacks.backend" content="AdonisJs, MariaDB, Redis, RabbitMQ" />
-    <Stack title="stacks.devops" content="Git, Docker, CI/CD, Traefik" />
+    <Stack title="stacks.devops" content="Git, Docker, CI/CD, Traefik, Kubernetes" />
   </SectionPart>
 </template>
